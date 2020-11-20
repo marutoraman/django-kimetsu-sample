7da3e0e4aa2e7ab8015785017cd497a46a608d72
newyee <takatelin01@icloud.com>
2020-11-20 19:03:25 +0900

    settings.pyの変更

--- kadai/settings.py
+++ kadai/settings.py
@@ -147,3 +147,6 @@ if not DEBUG:
     import django_heroku  # 追加
 
     django_heroku.settings(locals())  # 追加
+
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES["default"].update(db_from_env)
