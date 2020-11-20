bb72af3a15138b92a0b7f3de330ea7b4fdb67bf3
newyee <takatelin01@icloud.com>
2020-11-20 11:43:53 +0900

    settings.pyの変更

--- kadai/settings.py
+++ kadai/settings.py
@@ -23,7 +23,7 @@ except ImportError:
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(BASE_DIR)
+# print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -139,6 +139,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # 追加
 if not DEBUG:
